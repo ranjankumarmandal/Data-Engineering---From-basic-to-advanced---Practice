b8a66318ce7ec444acf0aabb48be8fcafaf84dfe
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-09-14 14:04:15 +0530

    Data Engineering: started

---
+++ Data_Engineering.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Data_Engineering {
+    public static void main(String[] args) {
+        System.out.println("Data Engineering: Spark, PySpark, Flink, and NiFi.")
+    }
+}
